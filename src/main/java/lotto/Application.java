45ada82b8359f1a753c5099989099246979f0d34
somsom13 <somin1113@naver.com>
2022-12-15 21:37:29 +0900

    fix(Application): 예외 처리 추가
    예외가 발생했을 때 예외 문장을 출력하고 어플리케이션을 정상 종료 한다.

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -5,6 +5,11 @@ import lotto.controller.LottoController;
 public class Application {
     public static void main(String[] args) {
         LottoController controller = new LottoController();
-        controller.start();
+        try {
+            controller.start();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+        }
+
     }
 }
